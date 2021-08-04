8c912e4e65d9038cf1b8670d263a6d916918d352
Marc-MEBEDO <73165179+Marc-MEBEDO@users.noreply.github.com>
2021-08-04 12:03:43 +0200

    Update kontakte.js

--- server/appdesign/products/crm/modules/kontakte.js
+++ server/appdesign/products/crm/modules/kontakte.js
@@ -6,6 +6,17 @@ export const Kontakte = {
     title: "Kontakte",
     description: "Alle Kontakte, die von uns benötigt werden.",
     faIconName: 'fa-fw far fa-address-card',
+    
+    namesAndMessages: {
+        singular: { mitArtikel: 'der Kontakt', ohneArtikel: 'Kontakt' },
+        plural: { mitArtikel: 'die Kontakte', ohneArtikel: 'Kontakte' },
+
+        // wenn vorhanden, dann wird die Message genutzt - ansonsten wird
+        // die Msg generisch mit singular oder plural generiert
+        messages: {
+
+        }
+    },
 
     fields: {
         title: { type: 'String', ...defaultSecurityLevel },
